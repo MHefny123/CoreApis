81428de7d1c117cca1222e613b058880b9bcc907
MHefny <mhefnydev@gmail.com>
2019-02-20 22:00:08 +0200

    FirstCommit

--- CoreApi/Startup.cs
+++ CoreApi/Startup.cs
@@ -70,6 +70,7 @@ namespace CoreApi
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                //
             }
 
             app.UseCors("AllowSpecificOrigin");
