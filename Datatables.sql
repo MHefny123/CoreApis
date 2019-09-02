411c038f9aac64c54f57a3196d6cecb420b43986
MHefny123 <mhefnydev@gmail.com>
2019-09-02 16:21:03 +0200

    dbSql edited by Product table

--- Datatables.sql
+++ Datatables.sql
@@ -1,4 +1,5 @@
-CREATE TABLE [dbo].[USERS](
+CREATE TABLE [dbo].[USERS]
+(
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](max) NULL,
 	[isActive] [bit] NULL
@@ -6,9 +7,23 @@ CREATE TABLE [dbo].[USERS](
 GO
 
 
-CREATE TABLE [dbo].[Dummies](
+CREATE TABLE [dbo].[Dummies]
+(
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](max) NULL,
 	[isActive] [bit] NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
+
+
+CREATE TABLE [dbo].[Product]
+(
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ProdName] [nvarchar](max) NULL,
+	[ProdDesc] [nvarchar](max) NULL,
+	[ProdPrice] [int] NULL,
+	[UpdatedAt] [DateTime] NULL
+)
+GO
+
+
